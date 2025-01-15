e32b9d3d221db7d9b9beb7b3f606e6fd8332ae10
Jasminedns <jasminedanese@icloud.com>
2025-01-15 17:13:42 +0100

    top 5 edited

--- scripts/api.js
+++ scripts/api.js
@@ -1,25 +1,31 @@
 $( () => { 
 
+    let allGames = [];
+
+
     const API_ENDPOINT = "https://api.rawg.io/api/games?";
     const apiKey = "f64cb459141649f18772e0841a1ca355";
 
     const getGames = async () => { 
-        let bestGames = [];
 
         for (let page = 1; page <= 10; page++) { 
             let response = await fetch(`${API_ENDPOINT}key=${apiKey}&page=${page}`); 
             let data = await response.json();
             
             let games = data.results;
-            best5Games = bestGames.concat(games);
+            allGames = allGames.concat(games);
 
-            best5Games.sort((a, b) => b.rating - a.rating);
+            allGames.sort((a, b) => b.rating - a.rating);
 
-            best5Games.forEach(game => {
-                $(".search__results").append(`${game.name} Rating: ${game.rating} <br>`);
+            let top5Games = allGames.slice(0, 5);
+
+            $(".search__results").empty();
+
+            top5Games.forEach(game => {
+                $(".search__results").append(`<img class="game__pic--img" src="${game.background_image}"> <p class="game__pic--text">${game.name}<br>Rating: ${game.rating}<br></p>`)
             });
         } 
-    } 
+    }
 
     $(".search-top5__window").on("click", () => {
         getGames();
